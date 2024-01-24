28c20f07c28cd0a630d3badc15f9d50772c4d5c0
Bivas Kumar <thetrotfreak@yahoo.com>
2024-01-24 15:24:21 +0530

    Update
    Remove author attribution

--- chatapplication/MulticastClient.java
+++ chatapplication/MulticastClient.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chatapplication;
 
 import static chatapplication.MulticastClient.name;
@@ -13,10 +8,6 @@ import java.net.*;
 import java.util.*;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Pinder
- */
 public class MulticastClient extends javax.swing.JFrame
 {
   public static String name;
